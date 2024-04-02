e6fc12b4e8340ea00c4cba2e3e919ea62477eeef
jAIval2 <64685988+jAIval2@users.noreply.github.com>
2024-04-02 20:40:57 +0530

    Update VotingSystemDAO.java

--- VotingSystemDAO.java
+++ VotingSystemDAO.java
@@ -1,23 +1,30 @@
 package voting;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class VotingSystemDAO {
-    private DBManager dbManager;
+
+    private Connection connection;
 
     public VotingSystemDAO() {
-        dbManager = new DBManager();
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/voting_system", "root", "hehe");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void insertVoterDetails(String hashedAge, String hashedVoterId, String party) {
+    public void insertVoterDetails(String hashedName, String hashedAge, String hashedVoterId, String party) {
         try {
-            Connection connection = dbManager.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO voters (hashed_age, hashed_voter_id, party_voted_for) VALUES (?, ?, ?)");
-            preparedStatement.setString(1, hashedAge);
-            preparedStatement.setString(2, hashedVoterId);
-            preparedStatement.setString(3, party);
+            String query = "INSERT INTO voters (hashed_name, hashed_age, hashed_voter_id, party_voted_for) VALUES (?, ?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, hashedName);
+            preparedStatement.setString(2, hashedAge);
+            preparedStatement.setString(3, hashedVoterId);
+            preparedStatement.setString(4, party);
             preparedStatement.executeUpdate();
             preparedStatement.close();
         } catch (SQLException e) {
@@ -26,6 +33,10 @@ public class VotingSystemDAO {
     }
 
     public void closeConnection() {
-        dbManager.closeConnection();
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
